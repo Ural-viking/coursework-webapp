6d885a8ee980c6954de2e1c8c31ea4cba6f3d07e
Ural-viking <i-didder@yandex.ru>
2023-01-12 10:15:35 +0500

    first commit

--- src/main/java/com/example/courseworkwebapp/controller/FilmController.java
+++ src/main/java/com/example/courseworkwebapp/controller/FilmController.java
@@ -2,6 +2,7 @@ package com.example.courseworkwebapp.controller;
 
 import com.example.courseworkwebapp.entity.Film;
 import com.example.courseworkwebapp.repository.FilmReposirory;
+import com.example.courseworkwebapp.service.UserActionService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -19,12 +20,15 @@ public class FilmController {
 
     @Autowired
     private FilmReposirory filmRepository;
+    @Autowired
+    private UserActionService userActionService;
 
     @GetMapping("/list")
     public ModelAndView getAllFilms(){
         log.info("/list -> connection");
         ModelAndView mav = new ModelAndView("list-films");
         mav.addObject("films",filmRepository.findAll());
+        userActionService.savelog("User get all film");
         return mav;
     }
 
@@ -33,12 +37,14 @@ public class FilmController {
         ModelAndView mav = new ModelAndView("add-film-form");
         Film film = new Film();
         mav.addObject("film", film);
+        userActionService.savelog("User add film");
         return mav;
     }
 
     @PostMapping("/saveFilm")
     public String saveFilm(@ModelAttribute Film film){
         filmRepository.save(film);
+        userActionService.savelog("User save film");
         return "redirect:/list";
     }
 
@@ -51,12 +57,14 @@ public class FilmController {
             film = optionalFilm.get();
         }
         mav.addObject("film",film);
+        userActionService.savelog("User show update form");
         return mav;
     }
 
     @GetMapping("/deleteFilm")
     public String deleteFilm(@RequestParam Long filmId){
         filmRepository.deleteById(filmId);
+        userActionService.savelog("User delete film");
         return "redirect:/list";
     }
 }
